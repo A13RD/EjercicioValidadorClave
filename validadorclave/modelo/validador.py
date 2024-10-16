0e5e6d2e328dd85a944cabbc3d391a4fe1a2e4bf
A13 <alejandroruizdiaz0221@gmail.com>
2024-10-16 13:46:41 -0500

    Inicio

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,13 @@
 # TODO: Implementa el código del ejercicio aquí
+
+from abc import abstractmethod, abstractclassmethod
+class Validador:
+    def __init__(self):
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+@abstractclassmethod
+class ReglaValidacion:
+

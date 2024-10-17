de243d523c8eddc16ecc40f1dc14509ae9cac8dd
A13RD <alejandroruizdiaz0221@gmail.com>
2024-10-16 19:46:56 -0500

    ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,6 +1,8 @@
 # TODO: Implementa el código del ejercicio aquí
 
-from abc import abstractmethod, abstractclassmethod
+from abc import abstractmethod, ABC
+
+
 class Validador:
     def __init__(self):
         pass
@@ -8,6 +10,24 @@ class Validador:
     def es_valida(self, clave: str) -> bool:
         pass
 
-@abstractclassmethod
-class ReglaValidacion:
 
+class ReglaValidacion(ABC):
+
+    def __init__(self, _longitud_esperada):
+        self._longitud_esperada: int = _longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(caracter.isupper() for caracter in clave)
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(caracter.islower() for caracter in clave)
+
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(caracter.isdigit() for caracter in clave)
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass

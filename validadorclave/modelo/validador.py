1346474eed80f1e724153c17816268e6fc59ddc3
A13RD <alejandroruizdiaz0221@gmail.com>
2024-10-16 21:55:12 -0500

    FinalEstructura

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,43 +1,75 @@
 # TODO: Implementa el código del ejercicio aquí
 
 from abc import abstractmethod, ABC
-
-
-class Validador:
-    def __init__(self):
-        pass
-
-    def es_valida(self, clave: str) -> bool:
-        pass
+from validadorclave.modelo.errores import *
 
 
 class ReglaValidacion(ABC):
 
-    def __init__(self, _longitud_esperada):
-        self._longitud_esperada: int = _longitud_esperada
+    def __init__(self, _longitud_esperada: int):
+        self._longitud_esperada = _longitud_esperada
 
     def _validar_longitud(self, clave: str) -> bool:
         return len(clave) > self._longitud_esperada
 
+    @staticmethod
     def _contiene_mayuscula(self, clave: str) -> bool:
-        return any(caracter.isupper() for caracter in clave)
+        return any(letra.isupper() for letra in clave)
 
+    @staticmethod
     def _contiene_minuscula(self, clave: str) -> bool:
-        return any(caracter.islower() for caracter in clave)
+        return any(letra.islower() for letra in clave)
 
+    @staticmethod
     def _contiene_numero(self, clave: str) -> bool:
-        return any(caracter.isdigit() for caracter in clave)
+        return any(letra.isdigit() for letra in clave)
 
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
         pass
 
 
-class ReglaValidadorCalisto(ReglaValidacion):
+class ReglaValidacionCalisto(ReglaValidacion):
 
     def contiene_calisto(self):
         pass
 
     def es_valida(self, clave: str) -> bool:
-        pass
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError
+        if not self.contiene_calisto(clave):
+            raise NoTienePalabraSecretaError
+        return True
 
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        caracter_especial = "@_#$%"
+        return any(letra in caracter_especial for letra in clave)
+
+    def es_valida(self, clave: str) -> bool:
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError
+
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError
+
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError
+
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError
+
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError
+
+        return True
+
+
+class Validador:
+
+    def es_valida(self, clave: str) -> bool:
+        pass
